da025b1a1ce92a3b1dc6704852edcf26d90e1576
NayKayvd <vandiemennahum@gmail.com>
2021-06-14 10:27:59 +0200

    updated

--- task2.py
+++ task2.py
@@ -6,16 +6,18 @@ window = tk.Tk()
 window.geometry("300x300")
 window.title("exception handling")
 
+
 def check():
     if int(amount_entry.get()) >= 3000:
-        raise messagebox.showinfo("status feedback","Congratulations. You qualify to go to Malaysia.")
+        raise messagebox.showinfo("status feedback", "Congratulations. You qualify to go to Malaysia.")
     else:
-        raise messagebox.showinfo("Status feedback","Please deposit more funds for this excursion.")
+        raise messagebox.showinfo("Status feedback", "Please deposit more funds for this excursion.")
+
 
 amount_label = Label(window, text='please enter your amount')
 amount_label.place(x=70, y=60)
 amount_entry = Entry(window)
-amount_entry.place(x=75,y=90)
+amount_entry.place(x=75, y=90)
 amount_button = Button(window, text='check qualification', command=check)
-amount_button.place(x=84, y= 150)
+amount_button.place(x=84, y=150)
 window.mainloop()
